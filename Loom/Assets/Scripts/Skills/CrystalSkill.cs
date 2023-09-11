1fb7139eaa972ed602c4b977465dbf0bfb65c937
Farzan Zand <farzanzand86@gmail.com>
2023-09-11 17:37:34 +0200

    Created multiple crystals that can be used to explode on target.

--- Loom/Assets/Scripts/Skills/CrystalSkill.cs
+++ Loom/Assets/Scripts/Skills/CrystalSkill.cs
@@ -15,11 +15,23 @@ public class CrystalSkill : Skill
     [SerializeField] private bool canMoveToEnemy;
     [SerializeField] private float moveSpeed;
 
+    [Header("Multi stacking crystal")]
+    [SerializeField] private bool canUseMultiStacks; 
+    [SerializeField] private int amountOfStacks;
+    [SerializeField] private float multiStackCooldown; // How long until you can cast multistack crystals again
+    [SerializeField] private float useTimeWindow; // How long you can cast crystals before ability goes on cooldown
+    [SerializeField] private List<GameObject> crystalsLeft = new List<GameObject>();
+
+    // Potential upgrade: Separate every skill in its own function to make it cleaaaan, cause this code is messy
+
     public override void UseSkill()
     {
         base.UseSkill();
 
-        // Use skill once, crystal spawns on you. Use skill again, you transform back to crystal and destroy it.
+        if (CanUseMultiCrystal()) 
+            return; 
+
+        // Use skill once, crystal spawns on you. Use skill again, you teleport back to crystal and destroy it.Place this in method to clean?
         if(currentCrystal == null)
         {
             currentCrystal = Instantiate(crystalPrefab, player.transform.position, Quaternion.identity);
@@ -38,5 +50,56 @@ public class CrystalSkill : Skill
             currentCrystal.transform.position = playerPos;
             currentCrystal.GetComponent<CrystalSkillController>()?.FinishCrystal();
         }
+    }
+
+    private bool CanUseMultiCrystal()
+    {
+        if (canUseMultiStacks)
+        {
+            if(crystalsLeft.Count > 0) // You start the scene with crystals loaded
+            {
+                if (crystalsLeft.Count == amountOfStacks) // Start the casting window when you cast first crystal
+                    Invoke("ResetAbility", useTimeWindow);
+
+                cooldown = 0; 
+                GameObject crystalToSpawn = crystalsLeft[crystalsLeft.Count - 1]; // Choose last crystal in list
+                GameObject newCrystal = Instantiate(crystalToSpawn, player.transform.position, Quaternion.identity);
+
+                crystalsLeft.Remove(crystalToSpawn);
+
+                newCrystal.GetComponent<CrystalSkillController>().
+                    SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed, FindClosestEnemy(newCrystal.transform));
+
+                if(crystalsLeft.Count <= 0) // If out of crystals, refill, and toggle cooldown
+                {
+                    cooldown = multiStackCooldown; // change cooldown from parent Skill.cs
+                    RefillCrystal(); 
+
+                }
+            return true;
+            }
+
+        }
+        return false;
+    }
+     
+
+    private void RefillCrystal()
+    {
+        int amountToAdd = amountOfStacks - crystalsLeft.Count; // Only add the amount we are missing from max stacks, to not go above it
+
+        for(int i = 0; i < amountToAdd; i++) 
+        {
+            crystalsLeft.Add(crystalPrefab);
+        }
+    }
+
+    private void ResetAbility() // This is created, but never called?
+    {
+        if (cooldownTimer > 0)
+            return; 
+
+        cooldownTimer = multiStackCooldown;
+        RefillCrystal();
     }
 }
