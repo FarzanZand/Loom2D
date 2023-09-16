44aa478008938dd987086f43b352ca3bf78d9340
Farzan Zand <Farzanzand86@gmail.com>
2023-09-16 21:30:18 +0200

    Added basic stat and modifier functionality.

---
+++ Loom/Assets/Scripts/Stat.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] // Will now be visible in inspector. Stat-object created will show all values.For instance, Stat damage or maxHealth;
+public class Stat
+{
+
+    [SerializeField] private int baseValue; 
+
+    public List<int> modifiers;
+
+    public int GetValue()
+    { 
+        int finalValue = baseValue;
+
+        foreach (int modifier in modifiers)
+        {
+            finalValue += modifier;
+        }
+        return finalValue; 
+    }
+
+    public void AddModifier(int _modifier)
+    {
+        modifiers.Add(_modifier);
+    }
+
+    public void RemoveModifier(int _modifier)
+    {
+        modifiers.RemoveAt(_modifier);
+    }
+}
