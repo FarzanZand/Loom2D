dfb33e68499757599a02437500aaeaf495dfe4bf
Farzan Zand <Farzanzand86@gmail.com>
2023-12-14 17:23:18 +0100

    Added the text boxes and created the core funcionality for the UI_StatSlot script

--- Loom/Assets/Scripts/Stats/CharacterStats.cs
+++ Loom/Assets/Scripts/Stats/CharacterStats.cs
@@ -1,7 +1,24 @@
 using System.Collections;
 using Unity.Hierarchy;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
-
+public enum StatType
+{
+    strength,
+    agility,
+    intelligence,
+    vitality,
+    damage,
+    critChance,
+    critPower,
+    health,
+    armor,
+    evasion,
+    magicRes,
+    fireDamage,
+    iceDamage,
+    lightningDamage
+}
 public class CharacterStats : MonoBehaviour
 {
     #region Damage Functionality
@@ -414,6 +431,26 @@ public class CharacterStats : MonoBehaviour
     #endregion
     protected virtual void Die()
     {
-        isDead = true; 
+        isDead = true;
+    }
+
+    public Stat GetStat(StatType _statType)
+    {
+        if (_statType == StatType.strength) return strength;
+        else if (_statType == StatType.agility) return agility;
+        else if (_statType == StatType.intelligence) return intelligence;
+        else if (_statType == StatType.vitality) return vitality;
+        else if (_statType == StatType.damage) return damage;
+        else if (_statType == StatType.critChance) return critChance;
+        else if (_statType == StatType.critPower) return critPower;
+        else if (_statType == StatType.health) return maxHealth;
+        else if (_statType == StatType.armor) return armor;
+        else if (_statType == StatType.evasion) return evasion;
+        else if (_statType == StatType.magicRes) return magicResistance;
+        else if (_statType == StatType.fireDamage) return fireDamage;
+        else if (_statType == StatType.iceDamage) return iceDamage;
+        else if (_statType == StatType.lightningDamage) return lightningDamage;
+
+        return null;
     }
 }
