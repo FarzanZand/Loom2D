92deea093017d6756d8b65b3900c07c06788e5b4
Farzan Zand <Farzanzand86@gmail.com>
2024-02-12 19:02:49 +0100

    Added the clone skill to the skill tree, and black hole too. Finished the skill tree skill connections. Also fixed a crafting bug so it takes away the right amount of materials.

--- Loom/Assets/Scripts/Skills/CloneSkill.cs
+++ Loom/Assets/Scripts/Skills/CloneSkill.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CloneSkill : Skill
 {
@@ -15,15 +16,77 @@ public class CloneSkill : Skill
     // 8. crystalInsteadOfClone, when CreateClone(), spawn crystal at start and return before clone is instantiated
 
     [Header("Clone info")]
+    [SerializeField] private float attackMultiplier; 
     [SerializeField] private GameObject clonePrefab;
-    [SerializeField] private float cloneDuration; 
+    [SerializeField] private float cloneDuration;
     [Space]
+
+    [Header("Clone attack")]
+    [SerializeField] private UI_SkillTreeSlot cloneAttackUnlockButton;
+    [SerializeField] private float cloneAttackMultiplier; 
     [SerializeField] private bool canAttack;
-    [Header("Clone can duplicate")]
+
+    [Header("Aggressive clone")]
+    [SerializeField] private UI_SkillTreeSlot aggressiveCloneUnlockButton;
+    [SerializeField] private float aggressiveCloneAttackMultiplier;
+    public bool canApplyOnHitEffect { get; private set; }
+
+    [Header("Multiple Clones")]
+    [SerializeField] private UI_SkillTreeSlot multipleUnlockButton;
+    [SerializeField] private float multiCloneAttackMultiplier;
     [SerializeField] private bool canDuplicateClone;
     [SerializeField] private float chanceToDuplicate;
     [Header("Crystal instead of clone")]
+    [SerializeField] private UI_SkillTreeSlot crystalInsteadUnlockButton;
     public bool crystalInsteadOfClone;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        cloneAttackUnlockButton.GetComponent<Button>().onClick.AddListener(UnlockCloneAttack);
+        aggressiveCloneUnlockButton.GetComponent<Button>().onClick.AddListener(UnlockAggressiveClone);
+        multipleUnlockButton.GetComponent<Button>().onClick.AddListener(UnlockMultiClone);
+        crystalInsteadUnlockButton.GetComponent<Button>().onClick.AddListener(UnlockCrystalInstead);
+    }
+
+    #region Unlock Region
+    private void UnlockCloneAttack()
+    {
+        if (cloneAttackUnlockButton.unlocked)
+        {
+            canAttack = true;
+            attackMultiplier = cloneAttackMultiplier;
+        }
+    }
+
+    private void UnlockAggressiveClone() // rename me? Yes
+    {
+        if(aggressiveCloneUnlockButton.unlocked)
+        {
+            canApplyOnHitEffect = true;
+            attackMultiplier = aggressiveCloneAttackMultiplier;
+        }
+    }
+
+    private void UnlockMultiClone()
+    {
+        if (multipleUnlockButton.unlocked)
+        {
+            canDuplicateClone = true;
+            attackMultiplier = multiCloneAttackMultiplier;
+        }
+    }
+
+    private void UnlockCrystalInstead()
+    {
+        if(crystalInsteadUnlockButton.unlocked)
+        {
+            crystalInsteadOfClone = true;
+        }
+    }
+
+    #endregion 
 
     public void CreateClone(Transform _clonePosition, Vector3 _offset)
     {
@@ -38,7 +101,7 @@ public class CloneSkill : Skill
 
         // Create a clone
             GameObject newClone = Instantiate(clonePrefab);
-        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration, canAttack, _offset, FindClosestEnemy(newClone.transform), canDuplicateClone, chanceToDuplicate, player);
+        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration, canAttack, _offset, FindClosestEnemy(newClone.transform), canDuplicateClone, chanceToDuplicate, player, attackMultiplier);
     }
 
     public void CreateCloneWithDelay(Transform _enemyTransform) // Called in ParrySkill.cs if talent is unlocked. 
